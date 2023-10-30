d3420be162e4eb342ecb6b4d2c7b419297470bf3
Greg Johnston <gregmj.sco@gmail.com>
2023-10-30 13:15:40 +0900

    Add html elements to header

--- react-shopping-cart/src/components/Header/Header.jsx
+++ react-shopping-cart/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
+import './Header.css'
+
 function Header() {
     return (
         <header>
-            <h1>This is the Header Title</h1>
+            <div>
+                <img src="" alt="header logo" />
+                <h1>This is the Header Title</h1>
+            </div>
+            <nav>
+                <a href="">How it works</a>
+                <a href="">Workflows</a>
+                <a href="">Download</a>
+                <a href="">Blog</a>
+            </nav>
         </header>
     )
 }
