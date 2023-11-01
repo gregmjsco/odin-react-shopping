def2fa05c77d402bdd822d19e8aaa84a1f14d61c
Greg Johnston <gregmj.sco@gmail.com>
2023-11-01 19:06:07 +0900

    Add Cart and USD buttons to header with basic styles applied

--- react-shopping-cart/src/components/Header/Header.jsx
+++ react-shopping-cart/src/components/Header/Header.jsx
@@ -3,16 +3,20 @@ import './Header.css'
 function Header() {
     return (
         <header>
-            <div>
-                <img src="" alt="header logo" />
-                <h1>This is the Header Title</h1>
+            <div className='header__left'>
+                <img className='header__left__logo' src="" alt="header logo" />
+                <h1 className='header__left__title'>monogram</h1>
             </div>
-            <nav>
-                <a href="">How it works</a>
-                <a href="">Workflows</a>
-                <a href="">Download</a>
-                <a href="">Blog</a>
+            <nav className='header__right'>
+                <a className='header__right__link' href="">How it works</a>
+                <a className='header__right__link' href="">Workflows</a>
+                <a className='header__right__link' href="">Download</a>
+                <a className='header__right__link' href="">Blog</a>
             </nav>
+            <div className='header__button__container'>
+                <button className='btn'>Cart</button>
+                <button className='btn'>USD</button>
+            </div>
         </header>
     )
 }
