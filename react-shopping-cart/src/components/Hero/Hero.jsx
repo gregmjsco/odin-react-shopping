1502f02873dca8c9a137a4b6e6d9274181dc3f76
Greg Johnston <gregmj.sco@gmail.com>
2023-11-11 14:51:33 +0900

    Add background image to hero section and set it to cover

---
+++ react-shopping-cart/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import './Hero.css'
+
+function Hero() {
+    return (
+        <section className="hero">
+            <h1 className="hero__title">Hello, World</h1>
+            <p className="hero__description">Filler Text for paragraph</p>
+        </section>
+    )
+}
+
+export default Hero;
