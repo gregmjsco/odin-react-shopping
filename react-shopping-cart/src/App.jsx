74305fc54bd0fa3373461659b319d916641b6cdd
Greg Johnston <gregmj.sco@gmail.com>
2023-11-11 14:45:36 +0900

    Add hero section folder and files into project with h1 and p in hero section

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
-  const [heading, setHeading] = useState("Magnificent Monkeys");
-
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
-
+  
   return (
     <>
       <Header />
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
+      <Hero />
     </>
   );
 };
