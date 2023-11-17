0b97f7dca89bde8bc178cc67dd16ff3829efad25
Greg Johnston <gregmj.sco@gmail.com>
2023-11-17 21:30:22 +0900

    Create btn utility class and apply to shop item buttons

--- react-shopping-cart/src/components/Shop/Shop.jsx
+++ react-shopping-cart/src/components/Shop/Shop.jsx
@@ -7,22 +7,22 @@ function Shop(){
                 <div className='shop__box shop__box--0margin'>
                     <h2 className='shop__box__title'>Product 1</h2>
                     <p className='shop__box__description'>This is the p for product 1</p>
-                    <button>Button 1</button>
+                    <button className='btn shop__box__btn'>Button 1</button>
                 </div>
                 <div className='shop__box'>
                     <h2 className='shop__box__title'>Product 2</h2>
                     <p className='shop__box__description'>This is the p for product 2</p>
-                    <button>Button 2</button>
+                    <button className='btn shop__box__btn'>Button 2</button>
                 </div>
                 <div className='shop__box'>
                     <h2 className='shop__box__title'>Product 3</h2>
                     <p className='shop__box__description'>This is the p for product 3</p>
-                    <button>Button 3</button>
+                    <button className='btn shop__box__btn'>Button 3</button>
                 </div>
                 <div className='shop__box'>
                     <h2 className='shop__box__title'>Product 4</h2>
                     <p className='shop__box__description'>This is the p for product 4</p>
-                    <button>Button 4</button>
+                    <button className='btn shop__box__btn'>Button 4</button>
                 </div>
             </div>
 
