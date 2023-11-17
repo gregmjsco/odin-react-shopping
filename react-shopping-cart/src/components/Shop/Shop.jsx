bfb04760549541f1a1ab7f5afd3c2ae299597833
Greg Johnston <gregmj.sco@gmail.com>
2023-11-17 21:23:38 +0900

    Reset button styles

--- react-shopping-cart/src/components/Shop/Shop.jsx
+++ react-shopping-cart/src/components/Shop/Shop.jsx
@@ -5,23 +5,23 @@ function Shop(){
         <section className="shop container">
             <div className="shop__grid-container">
                 <div className='shop__box shop__box--0margin'>
-                    <h2>Product 1</h2>
-                    <p>This is the p for product 1</p>
+                    <h2 className='shop__box__title'>Product 1</h2>
+                    <p className='shop__box__description'>This is the p for product 1</p>
                     <button>Button 1</button>
                 </div>
                 <div className='shop__box'>
-                    <h2>Product 2</h2>
-                    <p>This is the p for product 2</p>
+                    <h2 className='shop__box__title'>Product 2</h2>
+                    <p className='shop__box__description'>This is the p for product 2</p>
                     <button>Button 2</button>
                 </div>
                 <div className='shop__box'>
-                    <h2>Product 3</h2>
-                    <p>This is the p for product 3</p>
+                    <h2 className='shop__box__title'>Product 3</h2>
+                    <p className='shop__box__description'>This is the p for product 3</p>
                     <button>Button 3</button>
                 </div>
                 <div className='shop__box'>
-                    <h2>Product 4</h2>
-                    <p>This is the p for product 4</p>
+                    <h2 className='shop__box__title'>Product 4</h2>
+                    <p className='shop__box__description'>This is the p for product 4</p>
                     <button>Button 4</button>
                 </div>
             </div>
