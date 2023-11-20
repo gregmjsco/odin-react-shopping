07ffe2b0878ab9ae2fdfbc3349deeb2ffa5696ab
Greg Johnston <gregmj.sco@gmail.com>
2023-11-20 19:38:08 +0900

    Add placeholder shopping cart logic

--- react-shopping-cart/src/App.jsx
+++ react-shopping-cart/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Shop from "./components/Shop/Shop";
 import Footer from "./components/Footer/Footer";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
   
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Shop />
       <Footer />
+      <Cart />
     </>
   );
 };
--- react-shopping-cart/src/components/Cart/Cart.jsx
+++ react-shopping-cart/src/components/Cart/Cart.jsx
@@ -1,12 +1,49 @@
 import './Cart.css';
 
-function Cart(){
-    return (
-        <section className='cart'>
+import React, { useState } from 'react';
 
-            
-        </section>
-    )
-}
+const Cart = () => {
+  // State to manage the items in the cart
+  const [cartItems, setCartItems] = useState([]);
+
+  // Function to add an item to the cart
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  // Function to remove an item from the cart
+  const removeFromCart = (index) => {
+    const updatedCart = [...cartItems];
+    updatedCart.splice(index, 1);
+    setCartItems(updatedCart);
+  };
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+
+      {/* Render the items in the cart */}
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price}
+            <button onClick={() => removeFromCart(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+
+      {/* Example of items that can be added to the cart */}
+      <div>
+        <h3>Available Items</h3>
+        <button onClick={() => addToCart({ name: 'Item 1', price: 10 })}>
+          Add Item 1 to Cart
+        </button>
+        <button onClick={() => addToCart({ name: 'Item 2', price: 15 })}>
+          Add Item 2 to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
