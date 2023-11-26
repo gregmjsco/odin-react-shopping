94ec48738406c21ec664b5d2f4d23d009c0c7930
Greg Johnston <gregmj.sco@gmail.com>
2023-11-26 13:29:40 +0900

    Remove container class from hero section to have background image cover viewport width

--- react-shopping-cart/src/components/Hero/Hero.jsx
+++ react-shopping-cart/src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import './Hero.css'
 
 function Hero() {
     return (
-        <section className="hero container">
+        <section className="hero">
             <h1 className="hero__title">Hello, World</h1>
             <p className="hero__description">Filler Text for paragraph</p>
         </section>
